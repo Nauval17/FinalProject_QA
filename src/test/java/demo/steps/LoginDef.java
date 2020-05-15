a679997d2c89550639498e3dac6b083e007be3ea
Reynold Vinson Chen <reynold.vinson@gmail.com>
2020-05-15 18:19:53 +0700

    Login Page and StepDef

--- src/test/java/demo/steps/LoginDef.java
+++ src/test/java/demo/steps/LoginDef.java
@@ -1,33 +1,41 @@
 package demo.steps;
 
+import demo.pages.HomePage;
 import demo.pages.LoginPage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class LoginDef {
 
     LoginPage loginPage = new LoginPage();
-
+    HomePage homePage = new HomePage();
 
     @Given("User is on app login page")
     public void userIsOnAppLoginPage() {
+        boolean actual = loginPage.onLoginPage();
+        Assert.assertTrue(actual);
     }
 
     @When("User input {string} in phone number field")
-    public void userInputInPhoneNumberField(String loginPhoneNumber) {
+    public void userInputInPhoneNumberField(String loginPhoneNumber) { loginPage.inputPhoneNumber(loginPhoneNumber);
     }
 
     @When("User input {string} in password text field")
-    public void userInputInPasswordTextField(String loginPassword) {
+    public void userInputInPasswordTextField(String loginPassword) { loginPage.inputPassword(loginPassword);
     }
 
     @Then("User is on Home Page")
     public void userIsOnHomePage() {
+        boolean actual = homePage.onHomePage();
+        Assert.assertTrue(actual);
     }
 
     @Then("User is not on Home Page")
     public void userIsNotOnHomePage() {
+        boolean actual = homePage.onHomePage();
+        Assert.assertFalse(actual);
     }
 }
