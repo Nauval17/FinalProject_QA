e597a0b0970125abb282cc0ddd7ddeff905bfe60
Nauval Shafiq <nauval.shafiq@dana.id>
2020-05-17 01:33:51 +0700

    update apk v2

--- src/main/java/demo/driver/AndroidDriverInstance.java
+++ src/main/java/demo/driver/AndroidDriverInstance.java
@@ -23,7 +23,7 @@ public class AndroidDriverInstance {
         caps.setCapability("autoGrantPermissions", true);
         caps.setCapability("newCommandTimeout", 120);
         caps.setCapability("app",
-                System.getProperty("user.dir") + File.separator + "APP" + File.separator + "paystore+toast.apk");
+                System.getProperty("user.dir") + File.separator + "APP" + File.separator + "paystore rev1.apk");
         caps.setCapability("automationName", "UiAutomator2");
         try {
             androidDriver = new AndroidDriver<>(new URL(appiumUrl), caps);
